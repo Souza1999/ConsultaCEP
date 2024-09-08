32373db56ec1401c40929973f4f81bebc68ece35
Yuri Souza <bryurisouza@outlook.com.br>
2024-09-07 22:46:43 -0300

    Atualização da classe Endereco

--- app/src/main/java/br/edu/ifpb/pdm/yuri/consultacep/Endereco.kt
+++ app/src/main/java/br/edu/ifpb/pdm/yuri/consultacep/Endereco.kt
@@ -1,5 +1,17 @@
 package br.edu.ifpb.pdm.yuri.consultacep
 
-class Endereco {
-    // Criação do arquivo
-}
+data class Endereco (
+    val cep: String,
+    val logradouro: String,
+    val complemento: String,
+    val unidade: String,
+    val bairro: String,
+    val localidade: String,
+    val uf: String,
+    val estado: String,
+    val regiao: String,
+    val ibge: String,
+    val gia: String,
+    val ddd: String,
+    val siafi: String,
+)
