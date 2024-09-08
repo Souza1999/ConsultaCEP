95c712ba252b6f216b57b8e6cfb222fa17f20fb8
Yuri Souza <bryurisouza@outlook.com.br>
2024-09-07 23:14:43 -0300

    Atualização da interface EnderecoService

--- app/src/main/java/br/edu/ifpb/pdm/yuri/consultacep/EnderecoServiceIF.kt
+++ app/src/main/java/br/edu/ifpb/pdm/yuri/consultacep/EnderecoServiceIF.kt
@@ -1,2 +1,14 @@
 package br.edu.ifpb.pdm.yuri.consultacep
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface EnderecoServiceIF {
+    @GET
+        ("ws/{cep}/json/")
+    fun buscarEndereco(
+        @Path
+            ("cep") cep:String
+    ): Call<Endereco>
+}
